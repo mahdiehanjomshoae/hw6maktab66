5a2f4369a60cddf7dec44beab8fc1bf9afa66724
mahdieh <mahdieh.anjomshoae98@gmail.com>
2021-12-15 22:48:03 +0330

    add js Question1

---
+++ Question1/hw6-1.js
@@ -0,0 +1,15 @@
+function comper(s1, s2) {
+    let count = 0;
+    const arr1 = s1.split('');
+    const arr2 = s2.split('');
+    for (let i = 0; i < arr1.length; i++) {
+        let index = arr2.indexOf(arr1[i]);
+        if (index !== -1) {
+            arr2.splice(index, 1);
+            count += 1
+        }
+    }
+
+    return count
+}
+console.log(comper("aabcc", "adcaa")) 
